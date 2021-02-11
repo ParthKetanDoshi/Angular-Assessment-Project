ee14d9e6a3aeb3cb3e020a5a55bc7d47c5da0517
Parth D <parth.d@zycus.com>
2021-02-12 01:35:18 +0530

    fetch and display name and username on profile page

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { UserfetchService } from "./service/userfetch.service";
 
 @Component({
   selector: 'app-profile',
@@ -8,8 +9,15 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ProfileComponent implements OnInit {
   userid: number;
+  data: any;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute,
+    private userData:UserfetchService) { 
+      this.userData.getData().subscribe(userData => {
+        console.log(userData)
+        this.data=userData
+      })
+    }
 
   ngOnInit(): void {
     this.userid = this.route.snapshot.params['id'];
