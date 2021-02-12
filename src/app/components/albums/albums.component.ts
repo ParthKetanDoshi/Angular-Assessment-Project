cde0bca0aa589f2ea4b4593ade3665d8eff37761
Parth D <parth.d@zycus.com>
2021-02-12 16:03:34 +0530

    fetch album data

--- src/app/components/albums/albums.component.ts
+++ src/app/components/albums/albums.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { AlbumfetchService } from "./service/albumfetch.service";
 
 @Component({
   selector: 'app-albums',
@@ -7,8 +8,16 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class AlbumsComponent implements OnInit {
   @Input() userid: number;
+  albumData: any;
 
-  constructor() { }
+  constructor(private fetchedAlbumData: AlbumfetchService) { 
+    this.fetchedAlbumData.getData().subscribe(
+      fetchedAlbumData => {
+        // console.log(fetchedAlbumData)
+        this.albumData=fetchedAlbumData
+      }
+    )
+  }
 
   ngOnInit(): void {
     // console.log(this.userid);
