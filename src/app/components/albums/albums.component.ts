37d449b70590f84487526ce96369a686ccb9065c
Parth D <parth.d@zycus.com>
2021-02-12 16:09:53 +0530

    implement photo fetch data service in albums component

--- src/app/components/albums/albums.component.ts
+++ src/app/components/albums/albums.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { AlbumfetchService } from "./service/albumfetch.service";
+import { PhotofetchService } from './service/photofetch.service';
 
 @Component({
   selector: 'app-albums',
@@ -9,12 +10,20 @@ import { AlbumfetchService } from "./service/albumfetch.service";
 export class AlbumsComponent implements OnInit {
   @Input() userid: number;
   albumData: any;
+  photoData:any;
 
-  constructor(private fetchedAlbumData: AlbumfetchService) { 
+  constructor(private fetchedAlbumData: AlbumfetchService,
+    private fetchedPhotoData: PhotofetchService) { 
     this.fetchedAlbumData.getData().subscribe(
       fetchedAlbumData => {
         // console.log(fetchedAlbumData)
         this.albumData=fetchedAlbumData
+      }
+    )
+    this.fetchedPhotoData.getData().subscribe(
+      fetchedPhotoData => {
+        // console.log(fetchedPhotoData)
+        this.photoData=fetchedPhotoData
       }
     )
   }
