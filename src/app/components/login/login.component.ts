949c3117e5b12dca7b27ba8576acecc4d8f890b4
Parth D <parth.d@zycus.com>
2021-02-14 12:59:04 +0530

    pass logged in user id as string to local storage

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -7,6 +7,8 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  users: string[] = ["Sincere@april.biz","Shanna@melissa.tv","Nathan@yesenia.net","Julianne.OConner@kory.org","Lucio_Hettinger@annie.ca","Karley_Dach@jasper.info","Telly.Hoeger@billy.biz","Sherwood@rosamond.me","Chaim_McDermott@dana.io","Rey.Padberg@karina.biz"]
+  loggedinuserid: string;
 
   constructor(private router: Router) { }
 
@@ -14,14 +16,15 @@ export class LoginComponent implements OnInit {
   }
 
   onLogin(data){
-    if(data.userEmail == "Sherwood@rosamond.me" && data.password == "pass@123"){
+    if(this.users.indexOf(data.userEmail) != -1 && data.password == "pass@123"){
       // console.log("Login Successful")
-      localStorage.setItem('token','Sherwood@rosamond.me')
+      this.loggedinuserid = String(this.users.indexOf(data.userEmail) + 1);
+      localStorage.setItem('token',this.loggedinuserid);
       this.router.navigate([''])
     }
-    else{
-      // console.log("Login Failed")
-    }
+    // else{
+    //   // console.log("Login Failed")
+    // }
   }
 
 }
